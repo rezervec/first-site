14070f59e64bc4944d3cf56a14e3b3d6e829a5ae
gelik <rezervec@mail.ru>
2022-07-09 14:08:58 +0300

    Created Header and Menu

---
+++ header.js
@@ -0,0 +1,25 @@
+const town = document.querySelector('.header_town')
+const place = document.querySelector('.header_place')
+const car = document.querySelector('.header_car')
+const header = document.querySelector('.header')
+
+const header_width = header.getBoundingClientRect().right
+const header_height = header.getBoundingClientRect().bottom
+
+header.addEventListener('mousemove', (e) =>{
+    car.style.transform = `translate(${-e.pageX + e.pageX/1.03}px, ${-e.pageY + e.pageY/1.05}px)`
+    place.style.transform = `translate(${-e.pageX + e.pageX/1.02}px, ${-e.pageY + e.pageY/1.03}px)`
+    town.style.transform = `translate(${e.pageX - e.pageX/1.01}px, ${e.pageY - e.pageY/1.01}px)`
+});
+
+
+
+
+
+const navigation_ico = document.querySelector('.navigation_ico')
+const menu = document.querySelector('.navigation_content')
+
+navigation_ico.addEventListener('click', () =>{
+    navigation_ico.classList.toggle('_active');
+    menu.classList.toggle('_active')
+});
