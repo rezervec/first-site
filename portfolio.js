16bb888f73a0ecbaf0a2c827ab670ee5b662c57b
gelik <rezervec@mail.ru>
2022-07-17 21:15:00 +0300

    portfolio v1 (dev)

--- portfolio.js
+++ portfolio.js
@@ -34,9 +34,48 @@ if ((hidden_elems) > 0) {
 console.log(portfolio_elem.length)
 console.log( Math.floor(portfolio_window.clientWidth / portfolio_elem[0].clientWidth) )
 
+//////////
+// const modal_portfolio = document.querySelector('.modal_portfolio')
+// const modal_main = document.querySelector('.modal-main')
+// const modal_sub_el = document.querySelectorAll('.modal-sub-el')
+const cross = document.querySelector('.modal_portfolio span')
 
-// const modal_img = document.querySelectorAll('.modal_img')
 
-// modal_img.addEventListener('click', (e) => {
-//     console.log(e.target)
+// portfolio_elem.forEach(portfolio_elem => {
+//     portfolio_elem.addEventListener('click', (e) => {
+//         if(e.target.closest('img')) {
+//             modal_main.src = e.target.src;
+//             modal_sub_el.src = portfolio_elem.src;
+//             modal_portfolio.style.display = "block";
+//         }
+//         cross.addEventListener('click', () => {
+//             modal_portfolio.style.display = "none";
+//         });
+//     });
 // });
+
+const modal_portfolio = document.querySelector('.modal_portfolio');
+const modal_main = document.querySelector('.modal-main');
+const modal_sub_el = document.querySelectorAll('.modal-sub-el');
+
+
+for (i = 0; i < portfolio_elem.length; i++) {
+    portfolio_elem[i].addEventListener('click', (elem) => {
+        if (elem.target.closest('img')) {
+            modal_main.src = elem.target.src;
+            var element_click = document.querySelectorAll('.portfolio_elem')[i];
+            var element_images = document.querySelectorAll('.portfolio_elem img');
+            var modal_img_id = 0;
+            for (j = 0; j < 4; j++) {
+                if (element_images[j].img != elem.target) {
+                    modal_sub_el[modal_img_id].src = element_images[j].src;
+                }
+            }
+
+            modal_portfolio.style.display = "block";
+            cross.addEventListener('click', () => {
+                modal_portfolio.style.display = "none";
+            });
+        }
+    });
+}
